b43ff031846aab1c0c0ae721b9c419a817d61a32
ygqygq2 <ygqygq2@qq.com>
2025-04-30 14:34:37 +0800

    chore: 增加 shadcn ui
    Co-authored-by: ygqygq2 <ygqygq2@qq.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import { Button } from '@/components/ui/button';
+
 export default function Page() {
-  return <h1>Hello, Next.js!</h1>;
+  return (
+    <div>
+      <Button>Click me</Button>
+    </div>
+  );
 }
